9f2b8fb75c5c68560721cd3db180a22a9bea7ad6
syEadd <zec05024@naver.com>
2023-07-05 15:06:54 +0900

    🎉 매개변수 활용 법

---
+++ 2023.06.00/_AlertBox(매개변수)/all.js
@@ -0,0 +1,11 @@
+
+
+
+function openvom(매개변수){
+  document.getElementById('alert').style.display ='매개변수'
+}
+// html 에서 작성하야 되요..
+
+
+
+// document.getElementById('close-btn').addEventListener('click', closeAlert);
