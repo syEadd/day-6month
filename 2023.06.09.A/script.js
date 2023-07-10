dea3145abbb1fab5c94420f53ac5c4683d423ee0
syEadd <zec05024@naver.com>
2023-07-10 13:12:15 +0900

    💄 button 태그 설정

--- 2023.06.09.A/script.js
+++ 2023.06.09.A/script.js
@@ -1,15 +1,23 @@
 let buttons = document.querySelectorAll('.btn-group button');
 const all = 100;
 
+
 buttons.forEach(function(btn, index){
 
   btn.addEventListener('click',function(){
     let box = document.querySelector('.slide-list');
     let bune = -index * 100 + 'vw'
     // box.style.transform =  'translateX(' + bune + ')';
-    // btn.classList.add('att');
+    // btn.classList.toggle('att');
+    // 기능 1 ↑↑
     box.style.transform = `translateX(${bune})`;
     console.log(buttons);
-    // 수정 필요 !!document.querySelector('att').style.backgroundColor = 'red';
+    buttons.forEach(function(oterBtn){
+      oterBtn.classList.remove('att');
+    }) 
+    btn.classList.add('att');
+    // 기능 2 ↑↑
+    // const fpd = btn.document.querySelector('att').style.backgroundColor = 'red';
+  //  const dpd = document.querySelector('button').style.backgroundColor = 'red';
   })
 })
