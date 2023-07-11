c48c7855c5c41055075a05ae894fe13ea6910d10
syEadd <zec05024@naver.com>
2023-07-11 15:27:09 +0900

    🎉 메뉴판3

--- 2023.06.10/slide3/script(op)/fox2.js
+++ 2023.06.10/slide3/script(op)/fox2.js
@@ -3,6 +3,12 @@ console.log(all1)
 all1.forEach(function(itme){
   const sud = itme.querySelector('.sub')
   itme.addEventListener('mouseover',function(){
-    sud.classList.add('acc')
+    sud.classList.add('acc');
+    this.querySelector('a').classList.add('on');
+    
+    itme.addEventListener('mouseleave',function(){
+      sud.classList.remove('acc');
+      this.querySelector('a').classList.remove('on');
+    })
   })
 })
