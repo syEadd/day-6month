b3983cebd05378dfbd65054413220fb094557b6f
syEadd <zec05024@naver.com>
2023-07-12 17:08:15 +0900

    🚀  프로 ..

--- 2023.06.10/slide3/fox.js
+++ 2023.06.10/slide3/fox.js
@@ -1,9 +1,9 @@
 $('.gnb > li').hover(function(){
   $(this).find('ul').stop().slideDown(500);
   // hover 기능 다 보여줌 ↓
-  // $(this).find('a').addClass('on');
+  $(this).find('a').addClass('on');
 },function(){
 $(this).find('ul').stop().slideUp(500);
  // hover 기능 다 보여줌 ↓
-// $(this).find('a').removeClass('on');
+$(this).find('a').removeClass('on');
 })
--- 2023.06.10/slide3/script(op)/fox2.js
+++ 2023.06.10/slide3/script(op)/fox2.js
@@ -4,6 +4,7 @@ all1.forEach(function(itme){
   const sud = itme.querySelector('.sub')
   itme.addEventListener('mouseover',function(){
     sud.classList.add('acc');
+    // all1 건드리는 순간 a태그 전부 보여죠
     this.querySelector('a').classList.add('on');
     
     itme.addEventListener('mouseleave',function(){
