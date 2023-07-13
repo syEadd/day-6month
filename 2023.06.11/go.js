2dc79f21236909ebd1b038365c7867901d830759
syEadd <zec05024@naver.com>
2023-07-13 13:39:41 +0900

    💄 병원 리스트

--- 2023.06.11/go.js
+++ 2023.06.11/go.js
@@ -1,1 +1,13 @@
-const 
+const box =document.querySelectorAll('.text4 > li');
+console.log(box)
+
+
+box.forEach(function(all){
+  const fox1 = all.document.querySelector('.text5');
+
+  all.addEventListener('mouseover',function(){
+    fox1.classList.add('.acc');
+
+    
+  })
+})
