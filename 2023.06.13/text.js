9361b489bb0eea47db80bc7f706515d4ece021d4
syEadd <zec05024@naver.com>
2023-07-18 13:47:17 +0900

    ✨ setTimeout 기능 추가

---
+++ 2023.06.13/text.js
@@ -0,0 +1,8 @@
+const box1 = document.querySelector('.close-btn button');
+
+box1.addEventListener('click',function(){
+  setTimeout(function(){
+    document.querySelector('.popup').style.display = 'none'
+
+  }, 5000)
+})
