c2894b01cc64da5ee00ef7e86e0af23fdd34bf91
iingkejin <ingkejin@gmail.com>
2023-07-25 15:01:08 +0900

    💄 스타일 적용 메뉴

---
+++ 2023.06.25/all.js
@@ -0,0 +1,33 @@
+
+// $('.tab-item').eq(0).on('click',function(){
+//   $('.tab-item').removeClass('blue')
+//   $(this).addClass('blue');
+
+//   $('.tab-content').removeClass('show');
+//   $('.tab-content').eq(0).addClass('show')
+// })
+
+
+// $('.tab-item').eq(1).on('click',function(){
+//   $('.tab-item').removeClass('blue')
+//   $(this).addClass('blue');
+//   $('.tab-content').removeClass('show');
+//   $('.tab-content').eq(1).addClass('show');
+// })
+
+
+
+
+
+// for(let i = 0; i < $('.tab-item').length; i++) {
+//   $('.tab-item').eq(i).on('click', function(){
+//     $('.tab-item').removeClass('blue');
+//     $('.tab-content').removeClass('show');
+
+//     // $(this).addClass('blue');
+//     // = ↓ ↓ 같은 버전
+//     $('.tab-item').eq(i).addClass('blue');
+//     $('.tab-content').eq(i).addClass('show');
+//   })
+// }
+
