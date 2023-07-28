7b9a6d2b08bfd26532d473c92ed01d0c56df2028
iingkejin <ingkejin@gmail.com>
2023-07-28 16:56:35 +0900

    💄 네이버 스타일!!

--- 2023.06.27/jq/JQ.js
+++ 2023.06.27/jq/JQ.js
@@ -17,3 +17,47 @@ $('label').on('click', function(e){
   }
 })
 
+$('.total label').on('click', function(){
+  if($(this).children('.checkbox-img').hasClass('checked')) {
+    $('.agree').find('.checkbox-img').addClass('checked');
+    $('.agree').find('input[type="checkbox"]').attr('checked', true);
+  } else {
+
+    $('.agree').find('.checkbox-img').removeClass('checked');
+    $('.agree').find('input[type="checkbox"]').removeAttr('checked');
+  }
+})
+
+$('.agree label').on('click',function(){
+  let len = $('.agree .checkbox-img').length;
+  let chkien = $('.agree .checked').length;
+  let unchk = len - chkien;
+  console.log(chkien)
+  if(unchk == 0) {
+    $('.total label .checkbox-img').addClass('checked');
+    $('.total label input[type="checkbox"]').attr('checked', true);
+  } else {
+    $('.total label .checkbox-img').removeClass('checked');
+    $('.total label input[type="checkbox"]').removeAttr('checked');
+  }
+  
+})
+
+  $('#submit').on('click',function(e){
+    e.preventDefault()
+    let req = $('.req').length;
+    let req1 = $('.req .checked').length;
+
+    let unchk1 = req - req1;
+
+    if(unchk1 == 0){
+      $('#ido').submit()
+    }else{
+      e.preventDefault();
+      $('.req-alert').css('visibility', 'visible');
+    }
+})
+
+$('#cancel').on('click',function(){
+  location.href = 'https://www.naver.com/'
+})
