8a60d8e452cd2c4ab4999126621866efe66e62d1
syEadd <zec05024@naver.com>
2023-07-29 21:34:08 +0900

    🎉 NAVER.COM

--- 2023.06.26B/script.js
+++ 2023.06.26B/script.js
@@ -2,7 +2,7 @@ const allbtn = document.querySelector('.box1 img')
 const btn = document.querySelectorAll('.btn1 > img')
 let aa = 2;
 
-console.log(aa)
+
 allbtn.addEventListener('click',function(all){
   allbtn.classList.toggle('om')
   all.preventDefault()
@@ -13,24 +13,33 @@ allbtn.addEventListener('click',function(all){
  
   })
 })
-///////////////미저장
 
-let i = 2
-let a = btn.length
-console.log(i <= btn.length)
+
+
+
 btn.forEach(function(all){
-  all.addEventListener('click',function(){
-    if(this.on && this.on == 1 ){
-      
-      this.classList.add('on') 
-     // btn.classList.remove('on')
-    
 
+  all.addEventListener('click',function(){
+    
+    this.classList.toggle('on')
+    
+    let hot44 = document.querySelectorAll('.on').length;
+  
+    console.log(hot44)
+    if(3 == hot44){
+
+      allbtn.classList.add('om');
+      document.querySelector('.btn4').classList.remove('one');
+
+    }else if(2 == hot44){
+        allbtn.classList.remove('om')
+       document.querySelector('.btn4').classList.add('one')
     }else{
-      allbtn.classList.add('om')
-    
+      
+      allbtn.classList.remove('om')
     }
-    this.on = !this.on
+
     
+    })
+        
   })
- })
